1bea22feb80749338df0f931882a25471b17b95c
Ralph Yozzo <fedex1@gmail.com>
2013-10-19 22:22:54 -0400

    add sound logging

--- sound_logger_eclipse2/src/com/brooklynmarathon/sound_logger_eclipse2/MainActivity.java
+++ sound_logger_eclipse2/src/com/brooklynmarathon/sound_logger_eclipse2/MainActivity.java
@@ -2,17 +2,47 @@ package com.brooklynmarathon.sound_logger_eclipse2;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.media.MediaRecorder;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mStatusView = (TextView) findViewById(R.id.status);
+        Log.d(TAG, "QQQ: create: statusview: " + mStatusView);
         Intent intent = new Intent(this, RegisterActivity.class);
         startActivity(intent);
-        setContentView(R.layout.activity_main);
+        
+
+        if (runner == null)
+        { 
+            runner = new Thread(){
+                public void run()
+                {
+                    while (runner != null)
+                    {
+                        try
+                        {
+                            Thread.sleep(1000);
+                            Log.i("Noise", "Tock");
+                        } catch (InterruptedException e) { };
+                        mHandler.post(updater);
+                    }
+                }
+            };
+            runner.start();
+            Log.d("Noise", "start runner()");
+        }
+
+        
+       
     }
 
 
@@ -22,5 +52,124 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+    
+    TextView mStatusView;
+    MediaRecorder mRecorder;
+    Thread runner;
+    private static double mEMA = 0.0;
+    static final private double EMA_FILTER = 0.6;
+	private static final String TAG = MainActivity.class.getName();
+
+    final Runnable updater = new Runnable(){
+
+        public void run(){          
+            updateTv();
+        };
+    };
+    final Handler mHandler = new Handler();
+
+    public void XXXonCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //setContentView(R.layout.noiselevel);
+        //mStatusView = (TextView) findViewById(R.id.status);
+
+
+        if (runner == null)
+        { 
+            runner = new Thread(){
+                public void run()
+                {
+                    while (runner != null)
+                    {
+                        try
+                        {
+                            Thread.sleep(1000);
+                            Log.i("Noise", "Tock");
+                        } catch (InterruptedException e) { };
+                        mHandler.post(updater);
+                    }
+                }
+            };
+            runner.start();
+            Log.d("Noise", "start runner()");
+        }
+    }
+
+    public void onResume()
+    {
+        super.onResume();
+        try{
+        startRecorder();
+        }catch(Exception e){
+        	mStatusView.setText(e.getMessage());
+                	
+        }
+    }
+
+    public void onPause()
+    {
+        super.onPause();
+        stopRecorder();
+    }
+
+    public void startRecorder(){
+        if (mRecorder == null)
+        {
+            mRecorder = new MediaRecorder();
+            mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+            mRecorder.setOutputFile("/dev/null"); 
+            try
+            {           
+                mRecorder.prepare();
+            }catch (java.io.IOException ioe) {
+                android.util.Log.e("[Monkey]", "IOException: " + android.util.Log.getStackTraceString(ioe));
+
+            }catch (java.lang.SecurityException e) {
+                android.util.Log.e("[Monkey]", "SecurityException: " + android.util.Log.getStackTraceString(e));
+            }
+            try
+            {           
+                mRecorder.start();
+            }catch (java.lang.SecurityException e) {
+                android.util.Log.e("[Monkey]", "SecurityException: " + android.util.Log.getStackTraceString(e));
+            }
+
+            //mEMA = 0.0;
+        }
+
+    }
+    public void stopRecorder() {
+        if (mRecorder != null) {
+            mRecorder.stop();       
+            mRecorder.release();
+            mRecorder = null;
+        }
+    }
+
+    public void updateTv(){
+    	Log.d(TAG,"QQQ: " + mStatusView  + " " + Double.toString((getAmplitudeEMA())) + " dB" );
+    	
+    	mStatusView.setText(Double.toString((getAmplitudeEMA())) + " dB");
+    }
+    public double soundDb(double ampl){
+        return  20 * Math.log10(getAmplitudeEMA() / ampl);
+    }
+    public double getAmplitude() {
+        if (mRecorder != null)
+            return  (mRecorder.getMaxAmplitude());
+        else
+            return 0;
+
+    }
+    public double getAmplitudeEMA() {
+        double amp =  getAmplitude();
+        mEMA = EMA_FILTER * amp + (1.0 - EMA_FILTER) * mEMA;
+        return mEMA;
+    }
+
+    
     
 }
